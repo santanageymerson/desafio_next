e09a8aa4c4c22438969ca7c8a8cd5fafd07db82a
geymerson <santanageymerson@gmail.com>
2025-02-19 23:50:26 -0300

    criou lista e função adicionar

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaAmigos = []; 
+let nomeAmigo;
+
+function adicionarAmigo(){
+    
+    nomeAmigo = document.querySelector("input").value;
+    if (nomeAmigo == ''){
+        alert("Por favor, insira um nome ");
+    }
+    else{
+        listaAmigos.push(nomeAmigo);
+    }
+    limparCampo();
+    console.log(listaAmigos)
+
+
+}
+
+function limparCampo(){
+    nomeAmigo = document.querySelector('input');
+    nomeAmigo.value = '';
+}
+
+
